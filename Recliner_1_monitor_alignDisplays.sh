3bb6faf633e4a6931fd66331dba4ecec742533ad
Eric Baker <elb5465@gmail.com>
2022-03-24 16:31:25 -0400

    Create Recliner_1_monitor_alignDisplays.sh

---
+++ Recliner_1_monitor_alignDisplays.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+displayplacer "id:22C7239A-0FDE-EEC3-0284-9F3502E582D7 res:1792x1120 hz:59 color_depth:4 scaling:on origin:(0,0) degree:0" "id:ABD7611C-4707-C5E8-651C-A070BF0638C2 res:2560x1440 hz:59 color_depth:8 scaling:off origin:(-1792,-1440) degree:0"
+
