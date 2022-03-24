1cea2ae23cd2ecd940ef416ba8b370731c1c3738
Eric Baker <elb5465@gmail.com>
2022-03-24 16:30:24 -0400

    Create Desk_2_monitors_alignDisplays.sh

---
+++ Desk_2_monitors_alignDisplays.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+displayplacer "id:22C7239A-0FDE-EEC3-0284-9F3502E582D7 res:1792x1120 hz:59 color_depth:8 scaling:on origin:(0,0) degree:0" "id:51CAA4AD-9E14-1A64-0AB7-66E277FA57DE res:2560x1440 hz:59 color_depth:8 scaling:off origin:(-2560,-575) degree:0" "id:ABD7611C-4707-C5E8-651C-A070BF0638C2 res:2560x1440 hz:59 color_depth:8 scaling:off origin:(1792,-584) degree:0"
+
+
